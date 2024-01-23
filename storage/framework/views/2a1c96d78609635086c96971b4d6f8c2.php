caf53fe3c7ec2303c6b86ed1acf20bd4f7131860
bhagchandaniniraj <bhagchandani.niraj@gmail.com>
2024-01-23 09:58:27 +0530

    First Commit

--- storage/framework/views/2a1c96d78609635086c96971b4d6f8c2.php
+++
@@ -1,4 +0,0 @@
-<!-- Header Starts-->
-<h1>My Blog</h1>
-<?php echo $__env->make('templates.menu', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<!-- Header Ends --><?php /**PATH C:\Users\atmiya\Documents\Laravel\myapp\resources\views/templates/header.blade.php ENDPATH**/ ?>
